8350fd88aa97cf0b6e3ac9a5d585aef1e9c3b1bb
Ramzyayman <36305487+Ramzyayman@users.noreply.github.com>
2022-09-12 00:17:07 +0200

    Add files via upload

---
+++ app/script.js
@@ -0,0 +1,48 @@
+var body = document.getElementsByTagName("body")[0]
+var bgColorPicker1 = document.getElementById("colorPicker1")
+var bgColorPicker2 = document.getElementById("colorPicker2")
+var secondHalf = document.getElementById("secondHalf")
+
+
+
+bgColorPicker1.addEventListener("input", function(){
+    var color = bgColorPicker1.value;
+
+    body.setAttribute("style", "background-color: "+ color)
+    bgColorPicker1.setAttribute("style", "background-color: "+color)
+  }, false);
+
+  bgColorPicker2.addEventListener("input", function(){
+    var color = bgColorPicker2.value;
+
+    secondHalf.setAttribute("style", "background-color: "+ color)
+    bgColorPicker2.setAttribute("style", "background-color: "+color)
+  }, false);
+
+
+// || bgColorPicker2.getAttribute("class") == "hidden"
+// || bgColorPicker2.getAttribute("class") == "show"
+
+
+  body.onkeyup = function (key){
+      if (key.keyCode == 83) {
+
+        if (secondHalf.getAttribute("class") == "hidden"){
+            secondHalf.setAttribute("class", "secondhalf")
+        } else if (secondHalf.getAttribute("class") == "secondhalf") {
+            secondHalf.setAttribute("class", "hidden")
+        }
+          
+      }
+      else if (key.keyCode == 72) {
+        if (bgColorPicker1.getAttribute("class") == "hidden" || bgColorPicker2.getAttribute("class") == "hidden") {
+            bgColorPicker1.setAttribute("class", "show")
+            bgColorPicker2.setAttribute("class", "show")
+        } else if (bgColorPicker1.getAttribute("class") == "show" || bgColorPicker2.getAttribute("class") == "show") {
+            bgColorPicker1.setAttribute("class", "hidden")
+            bgColorPicker2.setAttribute("class", "hidden")
+        }
+    }
+  }
+
+
